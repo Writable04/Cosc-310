c403f846888f7b378a1e0c9d173f78f5b3a9c53a
Idan Lisman <idannlisman@gmail.com>
2026-02-28 18:07:07 -0800

    add user model and accounts db queries

--- src/app/db/storage_accounts.py
+++ src/app/db/storage_accounts.py
@@ -1,7 +1,22 @@
 from pathlib import Path
 from app.db.storage_base import Storage
+from app.models import UserInfo
+from fastapi import HTTPException
 
 class AccountsStorage(Storage):
     def __init__(self) -> None:
         path = Path(__file__).parent / "accounts.json"
         super().__init__(path)
+
+    def add_new_user(self, user: UserInfo) -> UserInfo:
+        if self.read(user.username) is not None:
+            raise HTTPException(status_code=400, detail="User already exists")
+
+        self.write(user.username, user)
+        return user
+
+    def get_user_info(self, username: str) -> UserInfo:
+        return self.read(username)
+    
+    def get_user_role(self, username: str) -> str:
+        return self.read(username).role
