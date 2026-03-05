ff401c3e149e197d2c072132c3a5a5a1c6bb55e4
Idan Lisman <idannlisman@gmail.com>
2026-03-04 20:49:45 -0800

    move require_auth to main so can be used in all routers

--- src/app/main.py
+++ src/app/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
 
 from app import __version__
+from app.repositories.storage_accounts import AccountsStorage
 from app.routers import resturants
 from app.routers import authentication
 from app.schemas.baseSchema import HealthResponse
+from app.services.authentication.auth import Authentication
 
 app = FastAPI(
     title="COSC 310 Project",
     version=__version__,
 )
 
+storage = AccountsStorage()
+authentication = Authentication(storage)
+
+
+def require_auth(username: str, token: str):
+    authentication.authenticate(username, token)
+    return True
 
 
 @app.get("/")
