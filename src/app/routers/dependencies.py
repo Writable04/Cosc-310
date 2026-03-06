39a3ba5f411d25401d57e81a775d586854ddab55
Idan Lisman <idannlisman@gmail.com>
2026-03-06 13:23:05 -0800

    enforce authorization for routes by role

--- src/app/routers/dependencies.py
+++ src/app/routers/dependencies.py
@@ -1,7 +1,7 @@
 from app.repositories.storage_accounts import AccountsStorage
 from app.services.authentication.auth import Authentication
 from app.services.authentication.registration import Registration
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Request
 
 router = APIRouter()
 
@@ -9,7 +9,13 @@ storage = AccountsStorage()
 authentication = Authentication(storage)
 registration = Registration(storage, authentication)
 
+admin_routes=['test/auth']
 
-def require_auth(username: str, token: str) -> bool:
+def require_auth(username: str, token: str, request: Request) -> bool:
     authentication.authenticate(username, token)
+    for admin_route in admin_routes:
+        route = request.scope.get("route").path
+        if admin_route in route and storage.get_account_role(username) != 'admin':
+            raise HTTPException(status_code=401, detail="Unauthorized")
+        
     return True
