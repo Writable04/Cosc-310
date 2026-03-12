251f6dcb563060d1bf7316dd161f0424b9363c5c
Idan Lisman <idannlisman@gmail.com>
2026-03-11 19:23:27 -0700

    rename api names - best practice

--- src/app/routers/dataset.py
+++ src/app/routers/dataset.py
@@ -18,64 +18,64 @@ me = MenuStorage()
 it = ItemStorage()
 
 #resturants
-@router.post("/postResturant")
+@router.post("/resturant")
 def post_resturant(resturant: Resturant):
     resturants.append(resturant.dict())
     res.new_resturant(resturant)
     return resturants
 
-@router.get("/getResturant/{restaurant_id}", response_model=Resturant)
+@router.get("/resturant/{restaurant_id}", response_model=Resturant)
 def get_resturant(restaurant_id: int):
     return res.find_resturant(restaurant_id)
 
-@router.put("/setResturant/{restaurant_id}")
+@router.put("/resturant/{restaurant_id}")
 def setResturant(restaurant_id: int, resturant: Resturant):
     res.update_resturant(restaurant_id, resturant.dict())
     return resturant.dict()
 
-@router.delete("/removeResturant/{restaurant_id}")
+@router.delete("/resturant/{restaurant_id}")
 def removeRestaurant(restaurant_id: int):
     res.remove_resturant(restaurant_id)
     return resturants
 
 #menus
-@router.post("/postMenu")
+@router.post("/menu")
 def post_menu(menu: Menu):
     menus.append(menu.dict())
     me.new_menu(menu)
     return menus
 
-@router.get("/getMenu/{menu_id}", response_model=Menu)
+@router.get("/menu/{menu_id}", response_model=Menu)
 def get_menu(menu_id: int):
     return res.find_menu(menu_id)
 
-@router.put("/setMenu/{menu_id}")
+@router.put("/menu/{menu_id}")
 def setMenu(menu_id: int, menu: Menu):
     me.update_menu(menu_id, menu.dict())
     return menu.dict()
 
-@router.delete("/removeMenu/{menu_id}")
+@router.delete("/menu/{menu_id}")
 def removeMenu(menu_id: int):
     me.remove_menu(menu_id)
     return menus
 
 #items
-@router.post("/postItem")
+@router.post("/item")
 def post_item(item: Item):
     items.append(item.dict())
     it.new_item(item)
     return items
 
-@router.get("/getItem/{item_id}", response_model=Item)
+@router.get("/item/{item_id}", response_model=Item)
 def get_item(item_id: int):
     return it.find_item(item_id)
 
-@router.put("/setItem/{item_id}")
+@router.put("/item/{item_id}")
 def setItem(item_id: int, item: Item):
     it.update_item(item_id, item.dict())
     return item.dict()
 
-@router.delete("/removeItem/{item_id}")
+@router.delete("/item/{item_id}")
 def removeItem(item_id: int):
     it.remove_item(item_id)
     return items
