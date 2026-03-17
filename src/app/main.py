7886464c745bb4d1c542a7171cd3ea6b9042530f
Gilles Fricker <gillesfricker@Gilless-MacBook-Air.local>
2026-03-17 15:49:02 -0700

    added query system

--- src/app/main.py
+++ src/app/main.py
@@ -4,7 +4,7 @@ from app import __version__
 from fastapi import FastAPI
 from app.routers import authentication
 from app.routers import notifications
-from app.routers import authentication, dataset
+from app.routers import authentication, dataset, querys
 from app.schemas.baseSchema import HealthResponse
 
 app = FastAPI(
@@ -18,5 +18,6 @@ def root() -> HealthResponse:
     return {"status": "ok", "version": __version__}
 
 app.include_router(dataset.router, prefix = "/dataset")
+app.include_router(querys.router, prefix = "/querys")
 app.include_router(authentication.router, prefix = "/authentication")
 app.include_router(notifications.router, prefix = "/notification")
