1d5036f60a5f90a02d5e1953a0b8c5853b3bce91
Idan Lisman <idannlisman@gmail.com>
2026-03-18 12:15:22 -0700

    fix authorization flow

--- src/app/routers/dependencies.py
+++ src/app/routers/dependencies.py
@@ -18,22 +18,23 @@ menu_storage = MenuStorage()
 item_storage = ItemStorage()
 cart = CartStorage()
 
-admin_routes=['test/auth', 'notification']
-resturant_manager_routes=[]
+admin_routes=['notification']
+resturant_manager_routes=['dataset']
 
 def require_auth(username: str, token: str, request: Request) -> bool:
     authentication.authenticate(username, token)
     route = request.scope.get("route").path
+    method = request.method.upper()
     role = accounts_storage.get_account_role(username)
     if (role == 'admin'):
         return True
-
+    
     for admin_route in admin_routes:
-        if admin_route in route:
+        if route.startswith(admin_route):
             raise HTTPException(status_code=401, detail="Unauthorized")
 
     for resturant_manager_route in resturant_manager_routes:
-        if resturant_manager_route in route and role != 'manager':
+        if role != 'manager' and route.startswith(resturant_manager_route) and method in {"POST", "PUT", "DELETE"}:
             raise HTTPException(status_code=401, detail="Unauthorized")
         
     return True
